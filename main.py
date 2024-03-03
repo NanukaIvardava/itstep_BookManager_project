6424ab3a9cd4b9565fcc66e57cce607ff5989c5b
NanukaIvardava <nana.ivardava@gmail.com>
2024-03-03 15:26:00 +0400

    added BookManager class

--- main.py
+++ main.py
@@ -8,3 +8,41 @@ class Book:
     # Showing book details
     def show_details(self):
         print(f"--📘--\nTitle: {self.title}\nAuthor: {self.author}\nPublication Year: {self.pub_year}\n")
+
+class BookManager:
+    # Constructor for the BookManager class, initializes book list
+    def __init__(self):
+        self.books = []
+
+    # adding a new book to the list
+    def add_book(self, title, author, pub_year):
+        new_book = Book(title.strip(), author.strip(), pub_year)
+        self.books.append(new_book)
+        print("\n✅ Book added successfully ✅\n")
+
+    # displaying all the books
+    def all_books(self):
+        if not self.books:
+            print("\n⛔ No books to display ⛔")
+        else:
+            print("\n🧾 List of all books:")
+            for book in self.books:
+                book.show_details()
+
+    # searching for a book by  title
+    def search_book(self, title):
+        for book in self.books:
+            if book.title.lower().strip() == title.lower().strip():
+                print("\n🔎 Book found:")
+                book.show_details()
+                return
+        print("\n⛔ Book not found ⛔")
+
+    # removing a book from the list
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title.lower().strip() == title.lower().strip():
+                self.books.remove(book)
+                print("\n✅ Book removed successfully ✅")
+                return
+        print("\n⛔ Book not found ⛔")
